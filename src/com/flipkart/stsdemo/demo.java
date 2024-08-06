539d275b6f60daed0afadaaba5b386aac8797a52
adwait-vk <adwait.v@flipkart.com>
2024-08-06 16:15:01 +0530

    First Commit

---
+++ src/com/flipkart/stsdemo/demo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.flipkart.stsdemo;
+
+/**
+ * 
+ */
+public class demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First STS Project");
+	}
+
+}
